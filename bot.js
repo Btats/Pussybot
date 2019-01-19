b7a46a9ffdd99efbe8da8eabfee317a238a462af
Btats <46830649+Btats@users.noreply.github.com>
2019-01-19 12:13:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,13 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+console.log("Scrpit By Dream");
 
-client.on('message', function(message) {
-                  if(!message.channel.guild) return;
-    if(message.content ===  ("!color 100")) {
-        if(message.member.hasPermission('MANAGE_ROLES')) {
-            setInterval(function(){})
-            message.channel.send('اصبر اخلص يبن المتناكة')
-        }else{
-            message.channel.send('معكش برمشن يبن الوسخة')
-            }
-    }
-});
 
-client.login("NTM2MDk3ODM2NzY1NDEzMzc2.DyRviQ.nmfchfgZjFwoiX62IAea7tgGTZY");
+client.on("ready", () => {
+let channel =     client.channels.get("536098376194719746")
+setInterval(function() {
+channel.send(`كسمكوا يمعرصين يولاد المتناكة ينعل ميتين امك انت وهو  يولاد العرص هنيكوا كلككوا يمعرصين و كسكم اي احد بيضحم او شرموط بيشرب جبنة يلا يولاد المتناكة`);
+}, 30)
+})
+
+client.login("NTM2MDk3ODM2NzY1NDEzMzc2.DySJLA.FdAgt4FS4X2XK3Uhcc375sE4Hg8");
